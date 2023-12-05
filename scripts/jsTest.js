e13a4d520f2d08f17874366467499132819915e4
How <abcd83129@gmail.com>
2023-12-06 00:23:34 +0800

    1.add page 4 to test .js file.

---
+++ scripts/jsTest.js
@@ -0,0 +1,29 @@
+function func1()
+{
+    console.log("hello, this is function 1.");
+    alert("Hello, this is function1.");
+}
+
+function func2()
+{
+    console.log("hello, this is function 2.");
+    alert("Hello, this is function2.");
+}
+
+function func3()
+{
+    console.log("hello, this is function 3.");
+    alert("Hello, this is function3.");
+}
+
+function func4()
+{
+    console.log("hello, this is function 4.");
+    alert("Hello, this is function4.");
+}
+
+function func5()
+{
+    console.log("hello, this is function 5.");
+    alert("Hello, this is function5.");
+}
